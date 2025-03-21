b17b54f199f04eae126c722e7764a0ab5b0d401a
ELISHA PASCUAL <ejoshpascual@gmail.com>
2025-03-21 17:23:38 +0800

    used the zfill function

--- 6_digit_format.py
+++ 6_digit_format.py
@@ -2,6 +2,7 @@ while True:
     try:
         nums = int(input("Enter a number between 0-1000: "))
         if nums in range(0, 1001):
+            numstr = str(nums) #converts the int to a string to make zfill function work
             break
         else:
             print("Try using numbers from 0 to 1000!")
@@ -9,4 +10,4 @@ while True:
     except ValueError:
         print("Try using numbers from 0 to 1000!")
 
-print (nums)
+print (numstr.zfill(6))
